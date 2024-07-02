57fa1999290d71e84d1b1afeb7c527ee9263ede5
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-07-02 13:15:54 +0200

    feat(swagger): add OpenAPI config

--- apps/api/src/main/java/fr/glowjob/hackaton/HackatonApplication.java
+++ apps/api/src/main/java/fr/glowjob/hackaton/HackatonApplication.java
@@ -1,8 +1,21 @@
 package fr.glowjob.hackaton;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Glowjob",
+		description = "Glowjob API's OpenAPI definitions"
+	),
+	servers = @Server(
+		url = "/",
+		description = "API"
+	)
+)
 @SpringBootApplication
 public class HackatonApplication {
 
