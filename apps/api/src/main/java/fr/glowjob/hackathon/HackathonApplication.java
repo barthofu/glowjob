0d8c854cf230a415f38344210fc9857d025cc17c
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-07-02 13:31:06 +0200

    feat(elastic): repo + model folders

--- apps/api/src/main/java/fr/glowjob/hackathon/HackathonApplication.java
+++ apps/api/src/main/java/fr/glowjob/hackathon/HackathonApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @OpenAPIDefinition(
@@ -19,6 +20,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "fr.glowjob.hackathon.repository")
+@EnableElasticsearchRepositories(basePackages = "fr.glowjob.hackathon.repository.elasticsearch")
 public class HackathonApplication {
 
 	public static void main(String[] args) {
