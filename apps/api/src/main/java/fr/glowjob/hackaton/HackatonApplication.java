025731a49d873df413cadbde8cb6db4f99649dc1
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-07-02 13:25:23 +0200

    feat(repo): add jpa repo

--- apps/api/src/main/java/fr/glowjob/hackaton/HackatonApplication.java
+++ apps/api/src/main/java/fr/glowjob/hackaton/HackatonApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @OpenAPIDefinition(
 	info = @Info(
@@ -17,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 	)
 )
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "fr.glowjob.hackaton.repository")
 public class HackatonApplication {
 
 	public static void main(String[] args) {
