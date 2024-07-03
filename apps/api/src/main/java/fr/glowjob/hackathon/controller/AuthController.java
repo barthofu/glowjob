ed32eaf6047433d02cb8d9f79e4d68da0e12c5aa
Noé <noe.favier@outlook.com>
2024-07-03 17:55:45 +0200

    feat(swag): swag 😎✨

--- apps/api/src/main/java/fr/glowjob/hackathon/controller/AuthController.java
+++ apps/api/src/main/java/fr/glowjob/hackathon/controller/AuthController.java
@@ -3,6 +3,7 @@ package fr.glowjob.hackathon.controller;
 import fr.glowjob.hackathon.model.dto.UserDto;
 import fr.glowjob.hackathon.model.dto.auth.UserSignupDto;
 import fr.glowjob.hackathon.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("auth/")
 @RequiredArgsConstructor
+@Tag(name = "login-endpoint")
 public class AuthController {
   private final UserService userService;
 
